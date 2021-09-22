e54ec6e5c8cfe0741a998502d5569f93e77da614
bonhamer <bonh248@deltalearns.ca>
2021-09-22 14:47:12 -0700

    1

--- task4.py
+++ task4.py
@@ -23,8 +23,8 @@ word = 'password'
 word2 = "Password"
 
 if word in sentence:
-    print('The sentence contains the password')
+    print('the sentence contains the password')
 elif word2 in sentence:
-    print("The sentence conatains the password")
+    print("the sentence conatains the password")
 elif word not in sentence:
-    print("The sentence does not contain password")
+    print("the sentence does not contain password")
