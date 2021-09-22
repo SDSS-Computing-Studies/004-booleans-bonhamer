f5f4b5097ce1767f94059743ab668f6a2a0145f9
bonhamer <bonh248@deltalearns.ca>
2021-09-22 14:48:36 -0700

    1

--- task3.py
+++ task3.py
@@ -32,11 +32,11 @@ Enter a number: 10000
 
 """
 number = float( input("Enter a number"))
-if number > 999:
+if number > 1000:
     print("3")
-elif 100 < number < 1000:
+elif 100 <= number < 1000:
     print("2")
-elif 10 < number < 100:
+elif 10 <= number < 100:
     print("1")
-elif 0 < number < 100:
+elif 0 <= number < 100:
     print("0")
